54770fe4f1d23662e158e5ac534fca2693b92e72
Lasse Haverinen <lasse.haverinen@gmail.com>
2023-11-02 12:08:30 +0200

    Test commit to test deployment to azure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Random Movie Quote App</h1>
-      <div>This app loads random movie quotes from HTTP API.  </div>
+      <div>This app loads random movie quotes from HTTP API.</div>
       <RandomMovieQuote count={3}/>
     </div>
   );
